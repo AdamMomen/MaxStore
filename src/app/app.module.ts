3e6f176c3c65dfe1f72056d9487ea9cb66e0e3bb
AdamMomen <adammuman81@gmail.com>
2020-01-15 14:57:55 +0100

    adding another side nav lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { ProductsComponent } from './components/products/products.component';
 import { CategroyComponent } from './components/categroy/categroy.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { ProductComponent } from './components/product/product.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+// I might use it later
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SimpleSidenavModule } from 'simple-sidenav';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,14 +19,16 @@ import { SimpleSidenavModule } from 'simple-sidenav';
     CategroyComponent,
     SideBarComponent,
     ProductComponent,
-    BrowserModule,
-    AppRoutingModule,
-    SimpleSidenavModule,
-    BrowserAnimationsModule
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
