3e26aee6b9947d638fccff71b943c82368462855
AdamMomen <adammuman81@gmail.com>
2020-01-15 11:37:58 +0100

    added produts - product - sidebar - user components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
-import { TestComponent } from './components/test/test.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategroyComponent } from './components/categroy/categroy.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    TestComponent
+    ProductsComponent,
+    CategroyComponent,
+    SideBarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './test.component';
+import { ProductsComponent } from './products.component';
 
-describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+describe('ProductsComponent', () => {
+  let component: ProductsComponent;
+  let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
+      declarations: [ ProductsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
